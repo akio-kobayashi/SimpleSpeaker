66d8ad9ce8f2c7b10a4177e9edd38817b3e7e858
akio-kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2021-05-18 20:49:24 +0900

    first commit

---
+++ train.py
@@ -0,0 +1,185 @@
+import os
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+import torch.optim as optim
+import torch.nn.functional as F
+#from torch.utils.tensorboard import SummaryWriter
+from model import ASRModel
+from generator import SpeechDataset
+import generator
+import numpy as np
+import argparse
+from sklearn.metrics import accuracy_score
+
+class IterMeter(object):
+    """keeps track of total iterations"""
+    def __init__(self):
+        self.val = 0
+
+    def step(self):
+        self.val += 1
+
+    def get(self):
+        return self.val
+
+def train(model, loader, optimizer, scheduler,
+          criterion, epoch, iter_meter):
+    model.train()
+
+    data_len=len(loader)
+
+    epoch_loss = 0
+    preds=None
+    corrs=None
+    for batch_idx, _data in enumerate(loader):
+        inputs, labels, lengths = _data
+
+        optimizer.zero_grad()
+
+        outputs = model(inputs.cuda(), lengths)
+        loss = criterion(outputs, labels.cuda())
+        loss.backward()
+
+        optimizer.step()
+        scheduler.step()
+        iter_meter.step()
+
+        labels=labels.unsqueeze(dim=-1)
+        outputs=outputs.to('cpu').detach().numpy().copy()
+        labels=labels.to('cpu').detach().numpy().copy().astype(float)
+        if preds is None:
+            preds=outputs
+            corrs=labels
+        else:
+            preds = np.vstack((preds, outputs))
+            corrs = np.vstack((corrs, labels))
+
+        if batch_idx > 0 and (batch_idx % 100 == 0 or batch_idx == data_len) :
+            print('Train Epcoh: {} [{}/{} ({:.0f}%)]\t Loss: {:.9f}'.format(
+                epoch, batch_idx * len(inputs), data_len*inputs.shape[0],
+                100. * batch_idx / data_len, loss.item())
+            )
+
+        epoch_loss += loss.item()
+
+        del loss
+        torch.cuda.empty_cache()
+
+    epoch_loss /= data_len
+
+    print('Train Epcoh: {}\t Loss: {:.9f}'.format(epoch, epoch_loss))
+    preds = np.argmax(preds, axis=1)
+    return epoch_loss, preds, corrs
+
+def evaluate(model, loader):
+
+    model.eval()
+    data_len = len(loader)
+    preds=None
+    corrs=None
+
+    with torch.no_grad():
+        for i, _data in enumerate(loader):
+            inputs, labels, lengths = _data
+
+            outputs = model(inputs.cuda(), lengths)
+            labels=labels.unsqueeze(dim=-1)
+
+            outputs=outputs.to('cpu').detach().numpy().copy()
+            labels=labels.to('cpu').detach().numpy().copy().astype(float)
+            if preds is None:
+                preds=outputs
+                corrs=labels
+            else:
+                preds = np.vstack((preds, outputs))
+                corrs = np.vstack((corrs, labels))
+                
+    preds = np.argmax(preds, axis=1)
+    return preds, corrs
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def initialize_weights(m):
+    if hasattr(m, 'weight') and m.weight.dim() > 1:
+        nn.init.xavier_uniform_(m.weight.data)
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', type=str, required=True, help='data(.h5)')
+    parser.add_argument('--train', type=str, required=True, help='training keys')
+    parser.add_argument('--valid', type=str, required=True, help='validation keys')
+    parser.add_argument('--out-stats', type=str, default='stats.h5', help='mean/std values for norm')
+    parser.add_argument('--batch-size', default=64, type=int, help='mini-batch size')
+    parser.add_argument('--epochs', default=10, type=int, help='training epochs')
+    parser.add_argument('--output', type=str, default='model', help='output model')
+    parser.add_argument('--learning-rate', type=float, default=1.0e-4, help='initial learning rate')
+    args = parser.parse_args()
+
+    use_cuda = torch.cuda.is_available()
+    torch.manual_seed(7)
+    device = torch.device("cuda" if use_cuda else "cpu")
+    if use_cuda is True:
+        print('use GPU')
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+    train_dataset=SpeechDataset(path=args.data, keypath=args.train, train=True)
+    mean, std = train_dataset.get_stats()
+
+    train_loader =data.DataLoader(dataset=train_dataset,
+                                  batch_size=args.batch_size,
+                                  shuffle=True,
+                                  collate_fn=lambda x: generator.data_processing(x,'train'),
+                                  **kwargs)
+
+    valid_dataset=SpeechDataset(path=args.data, keypath=args.valid, stats=[mean, std], train=False)
+    valid_loader=data.DataLoader(dataset=valid_dataset,
+                                batch_size=args.batch_size,
+                                shuffle=False,
+                                collate_fn=lambda x: generator.data_processing(x, 'valid'))
+
+    model=ASRModel()
+
+    print(f'The model has {count_parameters(model):,} trainable parameters')
+
+    model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(),
+                                lr = args.learning_rate,
+                                betas=(0.9, 0.98),
+                                eps=1.0e-9)
+
+    scheduler = optim.lr_scheduler.OneCycleLR(optimizer,
+                                              max_lr=args.learning_rate,
+                                              steps_per_epoch=int(len(train_loader)),
+                                              epochs=args.epochs,
+                                              anneal_strategy='linear')
+    criterion=nn.CrossEntropyLoss()
+    iter_meter=IterMeter()
+
+    max_acc=0.
+    for epoch in range(1, args.epochs+1):
+        loss, preds, corrs = train(model, train_loader,
+                                   optimizer, scheduler, criterion,
+                                   epoch, iter_meter)
+        acc = accuracy_score(corrs, preds)
+        print('Train Acc: %.3f' % acc)
+        # get predictions shape=(B, 1)
+        preds, corrs = evaluate(model, valid_loader)
+        acc = accuracy_score(corrs, preds)
+
+        print('Valid Acc: %.3f' % acc)
+
+        if acc > max_acc:
+            print('Maximum Acc changed... %.3f -> %.3f' % (max_acc , acc))
+            max_acc = acc
+            torch.save(model.to('cpu').state_dict(), args.output)
+            model.to(device)
+            
+    print('Maximum Acc: %.3f' % max_acc)
+
+if __name__ == "__main__":
+    main()
---
+++ xvector.py
@@ -0,0 +1,66 @@
+import os,sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import h5py
+from tdnn import TDNN
+
+def get_mask(tensor, lengths):
+    mask = np.zeros((tensor.shape[0], tensor.shape[1], 1))
+
+    for n in range(len(lengths)):
+        l=lengths[n]
+        if l > tensor.shape[1]:
+            l=tensor.shape[1]
+        mask[n:, :l, :] = 1.
+
+    return torch.from_numpy(mask.astype(np.float32)).clone()
+
+def stats_with_mask(tensor, mask):
+    mean = torch.div(torch.sum(tensor*mask, dim=1, keepdim=True),torch.sum(mask, dim=1, keepdim=True))
+    var = torch.square(tensor-mean)
+    var = torch.sum(var*mask, dim=1, keepdim=True)
+    var = torch.div(var, torch.sum(mask, dim=1, keepdim=True)+1.0e-8)
+    std = torch.sqrt(var)
+    if mean.shape[0] < 2:
+        mean, std = mean.squeeze(), std.squeeze()
+        mena,std = mean.unsqueeze(0), std.unsqueeze(0)
+    else:
+        mean, std = mean.squeeze(), std.squeeze()
+    return mean, std
+
+class X_vector(nn.Module):
+    def __init__(self, input_dim = 60, class_num=2):
+        super(X_vector, self).__init__()
+
+        self.tdnn1 = TDNN(input_dim=input_dim, output_dim=512, context_size=5, dilation=1,dropout_p=0.5)
+        self.tdnn2 = TDNN(input_dim=512, output_dim=512, context_size=3, dilation=2,dropout_p=0.5)
+        self.tdnn3 = TDNN(input_dim=512, output_dim=512, context_size=3, dilation=3,dropout_p=0.5)
+        self.tdnn4 = TDNN(input_dim=512, output_dim=512, context_size=4, dilation=4,dropout_p=0.5)
+        #### Frame levelPooling
+        self.segment5 = nn.Linear(512, 1500)
+        self.segment6 = nn.Linear(3000, 512)
+        self.segment7 = nn.Linear(512, 512)
+        self.output = nn.Linear(512, class_num)
+
+    def forward(self, inputs, lengths=None):
+        tdnn1_out = self.tdnn1(inputs)
+        tdnn2_out = self.tdnn2(tdnn1_out)
+        tdnn3_out = self.tdnn3(tdnn2_out)
+        tdnn4_out = self.tdnn4(tdnn3_out)
+        out = self.segment5(tdnn4_out) # (b, t, f)
+        ### Stat Pool
+        if lengths is None:
+            mean = torch.mean(out,1) # (b, f)
+            std = torch.std(out,1) # (b, f)
+        else:
+            mask=get_mask(out, lengths).cuda()
+            mean, std = stats_with_mask(out, mask)
+        stat_pooling = torch.cat((mean,std),1) # (b, fx2)
+        segment6_out = self.segment6(stat_pooling)
+        x_vec = self.segment7(segment6_out)
+        predictions = self.output(x_vec)
+
+        return predictions, x_vec
+
